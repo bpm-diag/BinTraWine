fb7d361fa8e89b266d2d32774f9df8702adb3cfe
Michele <m.manglaviti@araneum.it>
2023-05-23 18:28:42 +0200

    reboot blockchain

--- contract_addresses.js
+++ contract_addresses.js
@@ -1,8 +1,9 @@
-var AgronomoContractAddr = "0x19d0Cc2C5f059920a5Ba8d2528Cb530760e6D0fA";
-var ViticoltoreContractAddr = "0xF4Ed9463e3Fc1219CcaE3CC9eF2d0f5D593ccc62";
-var ProduttoreContractAddr = "0x39cd5e3832DcF893289B3E9905011441Dc5573C9";
-var ImbottigliatoreContractAddr = "0xA54A847acf1b4a3E24e1f44b9856fE74E776faCc";
-var DistributoreContractAddr = "0x88Dc5AFa2594FF227C88280728B2997873aFf651";
-var RivenditoreContractAddr = "0x512188EAC4aC3Fe7509c03Fb6406668E95BCFd0d";
-var EnteCertificatoreContractAddr = "0x4dB1C50a077b20770B8ea6f7910177c0363722c3";
-var CustomerContractAddr = "0xCE7bb8c0e71aBBD4cedFf2D4d27fd7D0C2F13FA7";
+
+  var AgronomoContractAddr = "0x60ED79B615Ec9aFD4250c94D67bCf8b67D07Dca1";
+  var ViticoltoreContractAddr = "0x825266F9d224db92fF25e2a0F90Da227a9C41bb1";
+  var ProduttoreContractAddr = "0xffBE41D09Cf146889512B96f38c677144f936F12";
+  var ImbottigliatoreContractAddr = "0xC3E5a3f8Cb0AD0736520A0066295733D70d92ba8";
+  var DistributoreContractAddr = "0xd72bd6897Ada03e02694e3ef3fA74566D63aFb6d";
+  var RivenditoreContractAddr = "0x60516F9b57AdFD0681B54682082faA53E37eEeF7";
+  var EnteCertificatoreContractAddr = "0x69e87592267B258EbcEa2414533d188A995DA417";
+  var CustomerContractAddr = "0x648f04D05F0D43e6DEfF4d0636f3962e8E829c6a";
