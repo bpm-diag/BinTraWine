ff2045c1123735f45af4cadab155e33da5dbcf7f
Fabio D'Adda <f.dadda4@campus.unimib.it>
2023-07-13 12:46:12 +0200

    delete all folder of bintrawine-vite

--- bintrawine-vite/src/layout/index.ts
+++
@@ -1,3 +0,0 @@
-import { _default } from './_default';
-
-export { _default };
--- bintrawine-vite/src/types/agronomo.ts
+++
@@ -1,7 +0,0 @@
-import * as z from 'zod';
-
-export const agronomoSchema = z.object({
-    token: z.string(),
-});
-
-export type Agronomo = z.infer<typeof agronomoSchema>;
